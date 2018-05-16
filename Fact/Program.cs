359de7b7f1c63c7c7fe92d872e1550b3b2b32a6c
Nick-FF <ngovsyannikov@gmail.com>
2018-05-16 20:55:56 +0300

    Add project files.

---
+++ Fact/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Fact
+{
+    class Program
+    {
+        private static object lockObject = new object();
+        static public float val = 1;
+        static public float count = 1;
+        static void Main(string[] args)
+        {
+        
+            ThreadPool.SetMinThreads(2, 2);
+            ThreadPool.SetMaxThreads(10, 10);
+
+            for (int i = 0; i < 10; i++)
+            {
+                
+               // Thread.CurrentThread.Name = Convert.ToString(i);
+                ThreadPool.QueueUserWorkItem(new WaitCallback(Metod));
+               
+            }
+            Console.ReadLine();
+       
+    }
+        static void Metod(object state)
+        {
+            lock(lockObject)
+            {
+               
+                val = val * count;
+                count++;
+            }
+            Console.WriteLine("Поток {0}, {1}", Thread.CurrentThread.Name, val);
+        }
+    }
+}
